939676639a9425923c79c3c539f56af037f4cbfa
johannbb <johannbb@stud.ntnu.no>
2025-08-20 11:20:13 +0200

    fix

--- johannebergan/src/app/page.tsx
+++ johannebergan/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="mx-auto max-w-5xl px-4 py-8">
       <h1 className="text-3xl font-semibold tracking-tight">Johanne Burns Bergan</h1>
       <div className="space-y-4">
-        Hei! Jeg heter Johanne :) 
+        Hei! Jeg heter Johanne 
         Jeg er 24 år og kommer fra Bærum. Jeg har fullført en bachelor i informatikk 
         ved NTNU i Trondheim, og skal nå begynne på en master i informatikk med retning programvaresystemer. 
         Denne høsten skal jeg på utveksling til Universitetet i Padova i Italia, sammen med to av mine beste venner.
